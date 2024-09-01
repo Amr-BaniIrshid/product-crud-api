b37a98b89bfc8d335c66ea17bd1bd0e3400b69ca
Amr-BaniIrshid <amramramrafg1688@gmail.com>
2024-09-02 00:39:04 +0300

    force commit

--- git2/src/main/java/com/example/productapi/product/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.productapi.product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
